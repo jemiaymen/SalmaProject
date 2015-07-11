e0d254faca0d1ed9e494b6a249d610372a6da2c3
aymen jemix <jemiaymen1@gmail.com>
2015-07-11 01:06:30 +0000

    Changes from Screen Builder

--- Bourse Academy Hybrid/courseadd/index.js
+++ Bourse Academy Hybrid/courseadd/index.js
@@ -5,13 +5,45 @@ app.courseadd = kendo.observable({
 });
 (function(parent) {
     var courseaddModel = kendo.observable({
-        fields: {
             img: '',
-            des: '',
+            desc: '',
             title: '',
+            fb: '', 
+            ct :'',  
+        
+        submit: function() {
+            var el = app.data.defaultProvider
+            var data = el.data('Corses');
+			var imgs = document.getElementById("img").files[0];
+            var file = {
+                "Filename":file.name,
+                "ContentType": file.type,
+            };
+            alert(file.type);
+            
+            el.Files.create(file,
+            function (data) {
+                alert(JSON.stringify(data));
+            },
+            function (error) {
+                alert(JSON.stringify(error));
+            });
+            
+            /*
+            data.create({ 'img' : courseaddModel.img ,'desc' : courseaddModel.desc ,'title' : courseaddModel.title  },
+                function(data){
+                    alert(JSON.stringify(data));
+                },
+                function(error){
+                    alert(JSON.stringify(error));
+                });
+                */
         },
-        submit: function() {},
-        cancel: function() {}
+        cancel: function() {
+            courseaddModel.img = null;
+            courseaddModel.des = '';
+            courseaddModel.title = '';
+        }
     });
 
     parent.set('courseaddModel', courseaddModel);
