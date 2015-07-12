7ba42398bebc6da75c96d9838ad21fe7b3fefb4f
aymen jemix <jemiaymen1@gmail.com>
2015-07-12 00:41:04 +0000

    Changes made by aymen jemix (jemiaymen1@gmail.com).

--- Bourse Academy Hybrid/courseadd/index.js
+++ Bourse Academy Hybrid/courseadd/index.js
@@ -1,17 +1,78 @@
 'use strict';
 
 app.courseadd = kendo.observable({
-    onShow: function() {}
+     onShow: function() {
+         /*
+        var au = app.data.defaultProvider.authentication.getAuthenticationStatus();
+        if(au._result != null){
+            if(au._result.status != "authenticated" ){
+             alert("you must Login !");
+             window.location.href = 'index.html';
+         } 
+        }
+         */
+    }
 });
 (function(parent) {
     var courseaddModel = kendo.observable({
-        fields: {
             img: '',
-            des: '',
+            desc: '',
             title: '',
+        validateData: function(data) {
+                if (!data.img || !data.desc || !data.title) {
+                    alert('Error All Fields are Required');
+                    return false;
+                }
+
+                return true;
+            },
+        submit: function() {
+            if(!courseaddModel.validateData(courseaddModel)) {
+                return ;
+            }
+            var el = app.data.defaultProvider
+            var data = el.data('Corses');
+			var imgs = document.getElementById("img").files[0];
+			var reader = new FileReader();
+            var file = {
+                    "Filename":imgs.name,
+                    "ContentType":imgs.type,
+                    "base64": ""
+                    };
+            reader.readAsDataURL(imgs);
+			reader.onload = function() {
+                file.base64 = reader.result.split(',')[1];
+                el.Files.create(file,
+                function (dt) {
+                    console.log( dt);
+                    data.create({ 'img' : dt.Id ,'desc' : courseaddModel.desc ,'title' : courseaddModel.title  },
+                    function(d){
+                        console.log( d);
+                        courseaddModel.rst();
+                    },
+                    function(err){
+                       console.log( err);
+                    });
+                },
+                function (rr) {
+                    console.log( rr);
+                });
+
+            };
+            
+            
+   			
+            
+            
+                
         },
-        submit: function() {},
-        cancel: function() {}
+        cancel: function() {
+            courseaddModel.rst();
+        },
+        rst : function(){
+          var frm = document.getElementById('frm');
+          frm.reset();  
+        }
     });
 
     parent.set('courseaddModel', courseaddModel);
